aeacaeb5bbed244fe9c63af96b2586016df459c0
huynhvantam <53110142+huynhvantam@users.noreply.github.com>
2020-05-13 15:58:27 +0700

    octyday13[webPhongBan]

---
+++ QLNV.Web/QLNV.Web/QLNV.Web/Controllers/PhongBanController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using QLNV.Web.Common;
+using QLNV.Web.Models;
+using QLNV.Web.Models.PhongBan;
+using System.Net;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace QLNV.Web.Controllers
+{
+    public class PhongBanController : Controller
+    {
+        public IActionResult Index()
+        {
+            var phongbans = new List<PhongBanView>();
+            var url = $"{Common.Common.ApiUrl}/phongban/danhsachphongban";
+            HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create(url);
+            httpWebRequest.Method = "GET";
+            var response = httpWebRequest.GetResponse();
+            {
+                string responseData;
+                Stream responseStream = response.GetResponseStream();
+                try
+                {
+                    StreamReader streamReader = new StreamReader(responseStream);
+                    try
+                    {
+                        responseData = streamReader.ReadToEnd();
+                    }
+                    finally
+                    {
+                        ((IDisposable)streamReader).Dispose();
+                    }
+                }
+                finally
+                {
+                    ((IDisposable)responseStream).Dispose();
+                }
+                phongbans = JsonConvert.DeserializeObject<List<PhongBanView>>(responseData);
+            }
+            return View(phongbans);
+
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
